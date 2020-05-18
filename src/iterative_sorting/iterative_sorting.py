c05d49a668f8c844787dc0a7cad41a8155ce11cc
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-18 16:25:11 -0500

    MVP is done and added comments all tests pass

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,7 +7,9 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
         minIndex = index + 1 
+        #start the loop at the minIndex and not at 0
         for minIndex in range(minIndex, len(arr)):
+            #if the element in this loop is smaller than the element in outer loop swap the smallest_index to be min
             if arr[minIndex] < arr[smallest_index]:
                 smallest_index = minIndex
 
@@ -22,8 +24,11 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    #start the loop at 1 to count for the length of the array and not the index
     for i in range(1, len(arr)):
+        #compare the element in current index to the element in the next slot over
         for index in range(len(arr) - 1):
+            #check to see if element at the current index is larger than the next elemenet, if it is trigger the bubble sort
             if arr[index] > arr[index + 1]:
                 temp = arr[index]
                 arr[index] = arr[index + 1]
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,30 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
-
+    #start at arr[0] and compare to the next element
+    for i in range(len(arr)):
+         #if target matches arr[i]
+        if target == arr[i]:
+             return i
+    return -1
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    #step 1 left = 0 right = length - 1 (right how many index we have)
+    left = 0
+    right = len(arr) - 1
+    #step 2 while loop through left <= right
+    while left <= right:
+        #step 3 find the midpoint of the array
+        midpoint = left + (right - 1) // 2
+        #step 4 compare target to arr[midpoint]
+        if arr[midpoint] == target:
+            #step 3 if it's a match return the index
+            return midpoint
+    #step 5 else if array[midpoint] < target move left
+        elif arr[midpoint] < target:
+            #left = mid + 1
+            left = midpoint + 1
+    #step 6 else move right
+        else:
+            #right = mid - 1
+            right = midpoint - 1
     return -1  # not found
